06f22af790250ad6d84441b8c10a01c0a94e612a
Chandan Saini <chandrab@observancegroup.com>
2025-05-29 23:41:59 +0530

    t2

--- otf/src/components/Login.js
+++ otf/src/components/Login.js
@@ -56,7 +56,7 @@ function Login() {
     <div
       style={{
         minHeight: '100vh',
-        background: 'linear-gradient(135deg, #43cea2 0%, #185a9d 100%)',
+        background: 'linear-gradient(135deg, #11998e 0%, #38ef7d 100%)',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
